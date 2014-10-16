50d81ddde20c4eab6b439d00e9cca167cf8476b7
Christian Rädel <christian.raedel@gmail.com>
2014-10-17 00:15:01 +0200

    Use bluebird for promises.

--- test/filemanager.spec.js
+++ test/filemanager.spec.js
@@ -1,27 +1,18 @@
 var filemanager = require('../lib/filemanager')
-    , TvDB      = require('../lib/tvdb')
-    , fs        = require('fs')
     , path      = require('path')
-    , q         = require('q')
     , chai      = require('chai')
     , expect    = chai.expect
-    , promised  = require('chai-as-promised')
-    , spies     = require('chai-spies')
     , _         = require('lodash');
 
-chai.use(promised).use(spies);
-
 describe('filemanager#find', function () {
-    it('should walk a directory', function (done) {
-        filemanager.find(process.cwd() + '/client', /^.*\.jsx$/g)
+    it('should find filenames', function (done) {
+        filemanager.find(path.resolve(process.cwd(), 'client'), /^.*\.jsx$/g)
+        .tap(console.log)
         .then(function (content) {
-            expect(content).to.be.ok;
-            console.log('summary:', content);
+            expect(content).to.be.an('array');
             done();
         })
-        .catch(function (err) {
-            done(new Error(err));
-        })
+        .catch(done)
         .done();
     });
 });
